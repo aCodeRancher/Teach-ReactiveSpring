46e7e85efec7f6dfaec0f372f73ca10389d58ae3
helen <hhma@alumni.cpp.edu>
2021-08-20 18:52:32 -0400

    create two tests for getting all items with two different approaches: consumeWith and StepVerifier.

--- learn-reactivespring/src/test/java/com/learnreactivespring/handler/ItemHandlerTest.java
+++ learn-reactivespring/src/test/java/com/learnreactivespring/handler/ItemHandlerTest.java
@@ -15,9 +15,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -61,6 +64,42 @@ public class ItemHandlerTest {
                 .expectBodyList(Item.class)
                 .hasSize(4);
 
+    }
+
+    @Test
+    public void getAllItems_approach2(){
+        webTestClient.get().uri(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(4)
+                .consumeWith((response) -> {
+                    List<Item> items =  response.getResponseBody();
+                    items.forEach((item) -> {
+                        assertTrue(item.getId()!=null);
+                    });
+
+                });
+    }
+
+    @Test
+    public void getAllItems_approach3(){
+
+
+        Flux<Item> itemsFlux = webTestClient.get().uri(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .returnResult(Item.class)
+                .getResponseBody();
+
+        StepVerifier.create(itemsFlux.log("values : "))
+                .expectNextMatches(item-> item.getDescription().equals("Samsung TV"))
+                .expectNextMatches(item -> item.getDescription().equals("LG TV"))
+                .expectNextMatches(item -> item.getDescription().equals("Apple Watch"))
+                .expectNextMatches(item -> item.getDescription().equals("Beats HeadPhones"))
+                .verifyComplete();
     }
 
     @Test
