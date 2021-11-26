e85de8011ba40a5f7b26a74d09ead08f642beb4f
helen <hhma@alumni.cpp.edu>
2021-11-26 11:34:22 -0500

    add wiremock tests.

--- item-client/src/test/java/com/learnreactivespring/controller/ItemClientControllerIT.java
+++ item-client/src/test/java/com/learnreactivespring/controller/ItemClientControllerIT.java
@@ -1,93 +1,172 @@
 package com.learnreactivespring.controller;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.learnreactivespring.domain.Item;
-import org.junit.Before;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.junit.Test;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
-import static org.junit.Assert.assertTrue;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
-@DirtiesContext
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+@AutoConfigureWireMock(port = 8084)
+@TestPropertySource(
+        properties = {
+                "restClient.baseUrl=http://localhost:8084"
+   }
+)
 public class ItemClientControllerIT {
+    @Autowired
+    WebTestClient webTestClient;
 
-    private WebClient webClient;
-    private static final String BASEURL= "http://localhost:8080";
+    @Test
+    void retreiveAll() {
 
-    @Before
-    public void setUp(){
-        webClient = WebClient.builder().baseUrl(BASEURL)
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.create().wiretap(true)))
-                .build();
+        stubFor(get(urlEqualTo("/v1/items"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("items.json")));
+
+        webTestClient.get().uri("/client/retrieve").exchange()
+                 .expectStatus().isOk()
+                .expectBodyList(Item.class)
+                 .consumeWith(list-> {
+                      List<Item> items= list.getResponseBody();
+                      assertTrue(items.size()==5);
+
+                 });
+
     }
 
     @Test
-    public void retrieveAllItems(){
-       Item item =  webClient.get().uri("/v1/items").accept(MediaType.APPLICATION_JSON)
-               .retrieve()
-               .bodyToFlux(Item.class).blockLast();
+    void exchangeAll() {
 
-       assertTrue(item.getDescription().equals("Beats HeadPhones"));
-     }
+        stubFor(get(urlEqualTo("/v1/items"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("items.json")));
 
-    @Test
-    public void exchangeAllItems() {
+        webTestClient.get().uri("/client/exchange").exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Item.class)
+                .consumeWith(list-> {
+                    List<Item> items= list.getResponseBody();
+                    assertTrue(items.size()==5);
 
-         Item item = webClient.get().uri("/v1/items").exchange()
-                .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Item.class)).blockLast();
-        assertTrue(item.getDescription()!=null);
+                });
+
     }
 
     @Test
-    public void retrieveOneItem(){
-        Item item =  webClient.get().uri("/v1/items/{id}", "ABC")
-                              .retrieve().bodyToMono(Item.class).block();
-        assertTrue(item.getDescription()!=null);
+    void retreiveOneItem() {
 
-   }
+       stubFor(get(urlEqualTo("/v1/items/1"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("item.json")));
 
-    @Test(expected = WebClientResponseException.class)
-    public void retrieveOneItemNotFound(){
+       webTestClient.get().uri("/client/retrieve/singleItem/1").exchange()
+                .expectStatus().isOk()
+                .expectBody(Item.class)
+                .consumeWith(item -> {
+                     Item  itemFound= item.getResponseBody();
+                    assertTrue(itemFound.getId().equals("1"));
+              });
 
-          webClient.get().uri("/v1/items/{id}", "ABCD")
-                 .retrieve().bodyToMono(Item.class).block();
+
+
+    }
+    @Test
+    void exchangeOneItem() {
+
+        stubFor(get(urlEqualTo("/v1/items/ABC"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("itemABC.json")));
+
+        webTestClient.get().uri("/client/exchange/singleItem").exchange()
+                .expectStatus().isOk()
+                .expectBody(Item.class)
+                .consumeWith(item -> {
+                    Item  itemFound= item.getResponseBody();
+                    assertTrue(itemFound.getId().equals("ABC"));
+                });
+
     }
 
     @Test
-    public void exchangeOneItem(){
-        Item item = webClient.get().uri("/v1/items/{id}", "ABC")
-                 .exchange().flatMap(clientResponse -> clientResponse.bodyToMono(Item.class)).block();
-         assertTrue(item.getDescription()!=null);
+    void createOneItem() {
+        Item itemToAdd = new Item("ABMac", "Mac Book Pro", 600.00);
+        String itemJson = "{\n" +
+                "  \"id\": \"ABMac\",\n" +
+                "  \"description\": \"Mac Book Pro\",\n" +
+                "  \"price\": 600.00\n" +
+                "}";
+        stubFor(post(urlEqualTo("/v1/items"))
+                .withRequestBody(equalToJson(itemJson))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("addOneItem.json")));
+
+        webTestClient.post().uri("/client/createItem")
+                .bodyValue(itemToAdd)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Item.class)
+                .consumeWith(item -> {
+                    Item  itemFound= item.getResponseBody();
+                    assertTrue(itemFound.getDescription().equals("Mac Book Pro"));
+                    assertTrue(itemFound.getId().equals("ABMac"));
+                });
+
     }
 
     @Test
-    public void createItem(){
-        Item laptop = new Item(null, "Lenovo laptop", 990.99);
-        Mono<Item> laptopMono = Mono.just(laptop);
-       Item addedItem = webClient.post().uri("/v1/items/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(laptopMono, Item.class)
-                .retrieve()
-                .bodyToMono(Item.class).block();
-        assertTrue(addedItem.getDescription().equals("Lenovo laptop"));
-   }
+    void createOneItemUsingExchange() {
+        Item itemToAdd = new Item("ABMac", "Mac Book Pro", 600.00);
+        String itemJson = "{\n" +
+                "  \"id\": \"ABMac\",\n" +
+                "  \"description\": \"Mac Book Pro\",\n" +
+                "  \"price\": 600.00\n" +
+                "}";
+        stubFor(post(urlEqualTo("/v1/items"))
+                .withRequestBody(equalToJson(itemJson))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("addOneItem.json")));
 
-   @Test
-    public void updateItem(){
-        Mono<Item> itemBody = Mono.just(new Item("ABC", "Beats HeadPhones", 139.99));
-        Item updatedItem = webClient.put().uri("/v1/items/{id}", "ABC")
-               .body(itemBody, Item.class)
-               .retrieve()
-               .bodyToMono(Item.class).block();
-         assertTrue(updatedItem.getPrice()==139.99);
-   }
+        webTestClient.post().uri("/client/exchange/createItem")
+                .bodyValue(itemToAdd)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Item.class)
+                .consumeWith(item -> {
+                    Item  itemFound= item.getResponseBody();
+                    assertTrue(itemFound.getDescription().equals("Mac Book Pro"));
+                    assertTrue(itemFound.getId().equals("ABMac"));
+                });
+
+    }
 }
+
