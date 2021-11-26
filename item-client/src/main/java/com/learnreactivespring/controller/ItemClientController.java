74f5b53d4270660f6a5766fafca6eae93c12720d
helen <hhma@alumni.cpp.edu>
2021-11-26 11:33:20 -0500

    add baseUrl and make the method testable in wiremock.

--- item-client/src/main/java/com/learnreactivespring/controller/ItemClientController.java
+++ item-client/src/main/java/com/learnreactivespring/controller/ItemClientController.java
@@ -2,6 +2,7 @@ package com.learnreactivespring.controller;
 
 import com.learnreactivespring.domain.Item;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -9,46 +10,52 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import java.util.List;
+
 
 @RestController
 @Slf4j
 public class ItemClientController {
 
-    WebClient webClient = WebClient.create("http://localhost:8080");
+    @Value("${restClient.baseUrl}")
+     private String baseUrl;
+
+    private WebClient webClient = WebClient.builder().build();
+
 
     @GetMapping("/client/retrieve")
-    public Flux<Item> getAllItemsUsingRetrieve(){
+    public Mono<List<Item>> getAllItemsUsingRetrieve() {
 
-        return webClient.get().uri("/v1/items")
-                .retrieve()
-                .bodyToFlux(Item.class)
-                .log("Items in Client Project retrieve : ");
+       return webClient.get().uri(baseUrl+"/v1/items")
+               .retrieve()
+               .bodyToFlux(Item.class)
+               .collectList();
     }
 
     @GetMapping("/client/exchange")
     public Flux<Item> getAllItemsUsingExchange(){
 
-        return webClient.get().uri("/v1/items")
+        return webClient.get().uri(baseUrl+ "/v1/items")
                 .exchange()
                 .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Item.class))
                 .log("Items in Client Project exchange : ");
     }
 
-    @GetMapping("/client/retrieve/singleItem/{id}")
-    public Mono<Item> getOneItemUsingRetrieve(@PathVariable String id){
+     @GetMapping("/client/retrieve/singleItem/{id}")
+    public Mono<Item> getOneItemUsingRetrieve(@PathVariable("id") String id){
 
-        return webClient.get().uri("/v1/items/{id}",id)
+        return webClient.get().uri(baseUrl+"/v1/items/{id}", id)
                 .retrieve()
                 .bodyToMono(Item.class)
                 .log("Items in Client Project retrieve single Item : ");
     }
 
-    @GetMapping("/client/exchange/singleItem")
-    public Mono<Item> getOneItemUsingExchange(){
+  @GetMapping("/client/exchange/singleItem")
+    public Mono<Item> getOneItemUsingExchange( ){
 
         String id = "ABC";
 
-        return webClient.get().uri("/v1/items/{id}",id)
+        return webClient.get().uri(baseUrl+"/v1/items/{id}",id)
                 .exchange()
                 .flatMap(clientResponse -> clientResponse.bodyToMono(Item.class))
                 .log("Items in Client Project retrieve single Item : ");
@@ -58,7 +65,7 @@ public class ItemClientController {
     public Mono<Item> createItem(@RequestBody Item item){
 
         Mono<Item> itemMono = Mono.just(item);
-       return webClient.post().uri("/v1/items")
+       return webClient.post().uri(baseUrl+"/v1/items")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(itemMono, Item.class)
                 .retrieve()
@@ -67,18 +74,18 @@ public class ItemClientController {
 
     }
 
-    @PostMapping("/client/exchange/createItem")
+     @PostMapping("/client/exchange/createItem")
     public Mono<Item> createItemExchange(@RequestBody Item item){
 
         Mono<Item> itemMono = Mono.just(item);
-        return webClient.post().uri("/v1/items").contentType(MediaType.APPLICATION_JSON)
+        return webClient.post().uri(baseUrl+"/v1/items").contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromPublisher(itemMono, Item.class))
                 .exchange().flatMap(clientResponse -> clientResponse.bodyToMono(Item.class))
                 .log("Created item is : ");
 
     }
 
-    @DeleteMapping("/client/deleteItem/{id}")
+    //@DeleteMapping("/client/deleteItem/{id}")
     public Mono<Void> deleteItem(@PathVariable String id){
 
         return webClient.delete().uri("/v1/items/{id}",id)
@@ -87,7 +94,7 @@ public class ItemClientController {
                 .log("Deleted Item is");
     }
 
-    @GetMapping("/client/retrieve/error")
+   // @GetMapping("/client/retrieve/error")
     public Flux<Item> errorRetrieve(){
 
         return webClient.get()
@@ -105,7 +112,7 @@ public class ItemClientController {
     }
 
 
-    @GetMapping("/client/exchange/error")
+    //@GetMapping("/client/exchange/error")
     public Flux<Item> errorExchange(){
 
         return webClient.get()
@@ -128,7 +135,6 @@ public class ItemClientController {
 
     }
 
-
     @PutMapping("/client/updateItem/{id}")
     public Mono<Item> updateItem(@PathVariable String id,
                                  @RequestBody Item item){
@@ -141,7 +147,4 @@ public class ItemClientController {
                 .bodyToMono(Item.class)
                 .log("Updated Item is : ");
     }
-
-
-
 }
